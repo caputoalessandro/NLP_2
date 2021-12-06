b811f281476c980c9d06a4ff37695c2d640d6981
acptIA <acpt1994@gmail.com>
2021-12-06 18:13:46 +0100

    annotazione funzionante

--- SWS/annotation.py
+++ SWS/annotation.py
@@ -2,21 +2,22 @@ import csv
 import os
 
 
-def get_mean_from_pairs(pairs):
+def get_means_from_pairs(pairs):
     return [(v1 + v2)/2 for v1, v2 in pairs]
 
 
+def get_rows(tsv):
+    with open(tsv) as t:
+        rows = csv.reader(t, delimiter="\t")
+        return list(map(tuple, rows))
+
+
 def get_pairs_values(fp_1, fp_2):
-    with open(fp_1) as f1:
-        rows = csv.reader(f1, delimiter="\t")
-        pairs = [(row[0], row[1]) for row in rows]
-        value_1 = [float(row[2]) for row in rows]
-
-    with open(fp_2) as f2:
-        rows = csv.reader(f2, delimiter="\t")
-        value_2 = [float(row[2]) for row in rows]
-
-    return pairs, [(v1, v2) for v1, v2 in zip(value_1, value_2)]
+    rows_1 = get_rows(fp_1)
+    rows_2 = get_rows(fp_2)
+    pairs = [(row[0], row[1]) for row in rows_1]
+    values = [(int(r1[2]), int(r2[2])) for r1, r2 in zip(rows_1, rows_2)]
+    return pairs, values
 
 
 def make_mean_file():
@@ -24,11 +25,11 @@ def make_mean_file():
     fp_2 = "resources/coppie_gentiletti.tsv"
 
     pairs, values = get_pairs_values(fp_1, fp_2)
-    means = get_mean_from_pairs(values)
+    means = get_means_from_pairs(values)
 
-    name = os.path.join("output", f"annotated_pairs.txt")
+    name = os.path.join("output", f"annotated_pairs.tsv")
     os.makedirs(os.path.dirname(name), exist_ok=True)
 
     with open(name, "w") as f:
-        for pair,  mean in zip(pairs, means):
-            f.write(str(pair)+str(mean))
+        for pair, mean in zip(pairs, means):
+            f.write(pair[0]+'\t'+pair[1]+'\t'+str(mean)+'\n')
